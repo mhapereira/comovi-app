caa8bb8ec2cd2621660d9710a605dd87fc10ffb3
Mateus Pereira <mhapereira99@gmail.com>
2024-08-30 02:28:53 -0300

    primeiro commit 2

--- database/migrations/2024_08_26_190143_create_contatos_table.php
+++ database/migrations/2024_08_26_190143_create_contatos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('contatos', function (Blueprint $table) {
             $table->id();
-            $table->string('email'); 
+            $table->string('email');
             $table->text('telefone');
             $table->timestamps();
         });
