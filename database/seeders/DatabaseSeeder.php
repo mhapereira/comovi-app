6defa85f9afc52eaf8c4d7187699905b5c57244f
Mateus Pereira <mhapereira99@gmail.com>
2024-09-04 17:04:19 -0300

    Ajuste CMS inclusão do Thema

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Mateus Pereira',
-            'email' => 'mhapereira99@gmail.com',
-            'password' => 'Mh@P!2$24*',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Mateus Pereira',
+        //     'email' => 'mhapereira99@gmail.com',
+        //     'password' => 'Mh@P!2$24*',
+        // ]);
 
         Theme::factory()->create([
             'primary_color' => '#ddd1cb',
@@ -29,9 +29,9 @@ class DatabaseSeeder extends Seeder
             'text_color' => '#1a1a1a',
         ]);
 
-        Contato::factory()->create([
-            'telefone' => '(16) 3251-1247',
-            'email' => 'comovi.melhoridade@hotmail.com',
-        ]);
+        // Contato::factory()->create([
+        //     'telefone' => '(16) 3251-1247',
+        //     'email' => 'comovi.melhoridade@hotmail.com',
+        // ]);
     }
 }
